2669cf149bc415a28249f6bb9c3c77b3063eb938
Andi Fajar Nugraha <andifajarlah@gmail.com>
2021-03-01 21:02:56 +0800

    coinpayment

--- app/Models/CoinpaymentTransactions.php
+++ app/Models/CoinpaymentTransactions.php
@@ -14,6 +14,6 @@ class CoinpaymentTransactions extends Model
 
     public function items()
     {
-        return $this->hasMany('App\Models\CoinPaymentTransactionItems', 'coinpayment_transaction_id', 'id');
+        return $this->hasMany('App\Models\CoinpaymentTransactionItems', 'coinpayment_transaction_id', 'id');
     }
 }
