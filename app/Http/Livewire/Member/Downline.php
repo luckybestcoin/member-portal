c300ed4861fdba637248cdfadc90916cfbac6ef8
Andi Fajar <andifajarlah@gmail.com>
2021-03-02 18:07:22 +0800

    member

--- app/Http/Livewire/Member/Downline.php
+++ app/Http/Livewire/Member/Downline.php
@@ -26,8 +26,7 @@ class Downline extends Component
         $data_member = Member::with('left_child')->with('right_child')->with('invalid_left_turnover')->with('invalid_right_turnover')->select("member_id", "member_email", "member_user",  "rating_id", "member_parent", "member_name", "member_position", "contract_price", "member_network", "due_date", "deleted_at",
         DB::raw('(select ifnull(sum(contract_price * extension), 0) from member a where a.member_password is not null and left(a.member_network, length(concat(member.member_network, member.member_id, "ki")))=concat(member.member_network, member.member_id, "ki") ) left_turnover'),
         DB::raw('(select ifnull(sum(contract_price * extension), 0) from member a where a.member_password is not null and left(a.member_network, length(concat(member.member_network, member.member_id, "ka")))=concat(member.member_network, member.member_id, "ka") ) right_turnover'))->where('member_id', $this->member)->first();
-        dd($data_member);
-        // dd($data_member);
+
         return view('livewire.member.downline', [
             'data' => $data_member
         ])
