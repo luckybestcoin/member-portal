b91f78d1038d08b54914942ab0a4c25aa625f9c2
Andi Fajar Nugraha <andifajarlah@gmail.com>
2021-04-01 06:40:46 +0800

    job

--- resources/views/email/recovery.blade.php
+++ resources/views/email/recovery.blade.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html>
 <body>
-    Hy {{ $name }}, this is your password recovery link:<br><br>
-    <a href="https://member.luckybestcoin.com/recovery?token={{ $token }}" target="_blank">https://member.luckybestcoin.com/recovery?token={{ $token }}</a><br>
+    Hy {{ $data['name'] }}, this is your password recovery link:<br><br>
+    <a href="https://member.luckybestcoin.com/recovery?token={{ $data['token'] }}" target="_blank">https://member.luckybestcoin.com/recovery?token={{ $data['token'] }}</a><br>
     Thank You
     <br><br>
     <div style="text-align: center">
-        This email was sent to {{ $email }}<br>
+        This email was sent to {{ $data['email'] }}<br>
         You received this email because you are registered with <a href="https://luckybestcoin.com">Lucky Best Coin</a>
     </div>
 </body>
 </html>
+
--- resources/views/email/registration.blade.php
+++ resources/views/email/registration.blade.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html>
 <body>
-    Hy {{ $name }}, welcome to Lucky Best Coin System. Please click to this link below to activate your account;<br><br>
-    <a href="https://member.luckybestcoin.com/referral?token={{ $token }}" target="_blank">https://member.luckybestcoin.com/referral?token={{ $token }}</a><br>
+    Hy {{ $data['name'] }}, welcome to Lucky Best Coin System. Click to this link below to activate your account;<br><br>
+    <a href="http://member.luckybestcoin.com/referral?token={{ $data['token'] }}" target="_blank">https://member.luckybestcoin.com/referral?token={{ $data['token'] }}</a><br>
+    Or use this referral code : {{ $data['token'] }}<br><br>
     Thank You
     <br><br>
     <div style="text-align: center">
-        This email was sent to {{ $email }}<br>
+        This email was sent to {{ $data['email'] }}<br>
         You received this email because you are registered with <a href="https://luckybestcoin.com">Lucky Best Coin</a>
     </div>
 </body>
 </html>
+
