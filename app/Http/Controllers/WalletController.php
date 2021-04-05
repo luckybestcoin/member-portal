6c56ae16b8cb5ac0597468226e108daf9ea72fad
Andi Fajar Nugraha <andifajarlah@gmail.com>
2021-04-05 21:30:41 +0800

    community

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -14,7 +14,7 @@ class WalletController extends Controller
     public function balance(Request $req)
     {
         $rate = new Rate();
-        $lbc_balance = bitcoind()->getbalance(auth()->user()->username)[0];
+        $lbc_balance = auth()->user()->username?bitcoind()->getbalance(auth()->user()->username)[0]: 0;
         $rate_dollar = $rate->last_dollar;
         return "LBC : ".$lbc_balance." = $ ".number_format($lbc_balance * $rate_dollar, 2);
     }
