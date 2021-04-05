5678f45fb0e619a902cebf2b4211730b52a28acc
Andi Fajar Nugraha <andifajarlah@gmail.com>
2021-04-05 19:37:22 +0800

    community

--- resources/views/email/recovery.blade.php
+++ resources/views/email/recovery.blade.php
@@ -2,12 +2,12 @@
 <html>
 <body>
     Hy {{ $data['name'] }}, this is your password recovery link:<br><br>
-    <a href="https://member.luckybestcoin.com/recovery?token={{ $data['token'] }}" target="_blank">https://member.luckybestcoin.com/recovery?token={{ $data['token'] }}</a><br>
+    <a href="https://member.luckybestcommunity.com/recovery?token={{ $data['token'] }}" target="_blank">https://member.luckybestcommunity.com/recovery?token={{ $data['token'] }}</a><br>
     Thank You
     <br><br>
     <div style="text-align: center">
         This email was sent to {{ $data['email'] }}<br>
-        You received this email because you are registered with <a href="https://luckybestcoin.com">Lucky Best Coin</a>
+        You received this email because you are registered with <a href="https://luckybestcommunity.com">Lucky Best Coin</a>
     </div>
 </body>
 </html>
--- resources/views/email/registration.blade.php
+++ resources/views/email/registration.blade.php
@@ -2,13 +2,13 @@
 <html>
 <body>
     Hy {{ $data['name'] }}, welcome to Lucky Best Coin System. Click to this link below to activate your account;<br><br>
-    <a href="http://member.luckybestcoin.com/referral?token={{ $data['token'] }}" target="_blank">https://member.luckybestcoin.com/referral?token={{ $data['token'] }}</a><br>
+    <a href="http://member.luckybestcommunity.com/referral?token={{ $data['token'] }}" target="_blank">https://member.luckybestcommunity.com/referral?token={{ $data['token'] }}</a><br>
     Or use this referral code : {{ $data['token'] }}<br><br>
     Thank You
     <br><br>
     <div style="text-align: center">
         This email was sent to {{ $data['email'] }}<br>
-        You received this email because you are registered with <a href="https://luckybestcoin.com">Lucky Best Coin</a>
+        You received this email because you are registered with <a href="https://luckybestcommunity.com">Lucky Best Coin</a>
     </div>
 </body>
 </html>
