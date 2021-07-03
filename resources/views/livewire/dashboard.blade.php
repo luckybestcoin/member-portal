98caefd1c7391820b31e43284579f49fbff3cf01
Andi Fajar Nugraha <andifajarlah@gmail.com>
2021-07-03 09:08:37 +0700

    baru

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -87,14 +87,14 @@
                                 </h5>
                             </div>
                             @else
-                            {{-- @if ($now  < $open)
+                            @if ($now  < $open)
                             <div class="alert alert-warning text-center">
                                 <h4><small>You can convert you remaining contract to HEBA on :</small> <br><br> July 5, 2021<br>8:00 am UTC<br>
                                     (15:00 Western Indonesian Time)
                                     <br><br>
                                     <small>Don't Miss It!!!</small></h4>
                             </div>
-                            @else --}}
+                            @else
                             <form wire:submit.prevent="submit">
                                 <div class="card">
                                     <div class="card-body">
