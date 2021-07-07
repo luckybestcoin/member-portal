2f30349020fdfeb645c37bab770f38d594df4d5f
Andi Fajar Nugraha <andifajarlah@gmail.com>
2021-07-07 14:22:39 +0800

    dashboard

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -351,6 +351,9 @@ class Dashboard extends Component
         $this->daily = $trx_reward->where('transaction_reward_type', 'Daily')->where('member_id', auth()->id())->get()->sum('transaction_reward_amount');
         $this->fee = $trx_pin->balance;
         $this->heba = (auth()->user()->contract_price * 3) - $this->trx_exchange_reward == 0? 0:ceil(((auth()->user()->contract_price * 3) - $this->trx_exchange_reward) / 0.051724138);
+        Member::where('member_id', auth()->id())->update([
+            'heba' => $this->heba
+        ]);
         return view('livewire.dashboard')
         ->extends('livewire.main', [
             'breadcrumb' => [],
