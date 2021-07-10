bdc6850afb734b1bebb7092350bd61e9e4b46ce6
Andi Fajar Nugraha <andifajarlah@gmail.com>
2021-07-10 23:49:14 +0800

    baru

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -303,7 +303,7 @@ class Dashboard extends Component
                     $header = ["X-Auth-Apikey" => "53bd98f87ba331f1", "X-Auth-Nonce" => $nonce, "X-Auth-Signature" => hash_hmac("SHA256", $nonce."53bd98f87ba331f1", "36f77e717cdadacba1a8dce95ce8ba66") ];
                     $response = Http::withHeaders($header)->post('https://www.digiassetindo.com/api/v2/exchange/account/account_transfers', [
                         'currency' => 'HEBA',
-                        'amount' => 1,
+                        'amount' => $this->heba - $wd,
                         'username_or_uid' => $this->uid,
                     ])->json();
                     if (array_key_exists('created_at', $response)) {
