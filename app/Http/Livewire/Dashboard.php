f6fcaef235c159521cb6f4c1097d71e24001f86c
Andi Fajar Nugraha <andifajarlah@gmail.com>
2021-07-10 23:46:40 +0800

    barug

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -10,6 +10,7 @@ use App\Models\Member;
 use Livewire\Component;
 use App\Models\Achievement;
 use App\Models\Transaction;
+use Illuminate\Support\Str;
 use App\Models\TransactionReward;
 use Illuminate\Support\Facades\DB;
 use App\Models\TransactionExchange;
@@ -302,10 +303,10 @@ class Dashboard extends Component
                     $header = ["X-Auth-Apikey" => "53bd98f87ba331f1", "X-Auth-Nonce" => $nonce, "X-Auth-Signature" => hash_hmac("SHA256", $nonce."53bd98f87ba331f1", "36f77e717cdadacba1a8dce95ce8ba66") ];
                     $response = Http::withHeaders($header)->post('https://www.digiassetindo.com/api/v2/exchange/account/account_transfers', [
                         'currency' => 'HEBA',
-                        'amount' => $this->heba - $wd,
+                        'amount' => 1,
                         'username_or_uid' => $this->uid,
                     ])->json();
-                    if ($response == null) {
+                    if (array_key_exists('created_at', $response)) {
                         $this->amount = ((auth()->user()->contract_price * 3 * auth()->user()->extension) - $this->trx_exchange_reward);
                         $this->heba = ceil($this->amount / 0.051724138) - $wd;
 
@@ -320,7 +321,7 @@ class Dashboard extends Component
 
                         DB::transaction(function () use($wd) {
                             $information = "Conversion reward $ ".$this->amount." to ".($this->heba - $wd). " HEBA";
-                            $id = bitcoind()->getaccountaddress(auth()->user()->username).date('Ymdhis').round(microtime(true) * 1000);
+                            $id = Str::random(10).date('Ymdhis').round(microtime(true) * 1000);
 
                             $transaksi = new Transaction();
                             $transaksi->transaction_id = $id;
@@ -337,41 +338,8 @@ class Dashboard extends Component
                         });
                         redirect("/");
                     }
-                    if (!array_key_exists('status', $response)) {
-                        $this->error = $response['errors']? $response['errors'][0]: 'Conversion failed. Try again later';
-                        return;
-                    }
-                        $this->amount = ((auth()->user()->contract_price * 3 * auth()->user()->extension) - $this->trx_exchange_reward);
-                        $this->heba = ceil($this->amount / 0.051724138) - $wd;
-
-                        $this->done = now();
-
-                        Member::where('member_id', auth()->id())->update([
-                            'converted_at' => $this->done,
-                            'heba' => $this->heba - $wd,
-                            'uid' => $this->uid
-                        ]);
-                        Tron::where('uid', $this->uid)->delete();
-
-                        DB::transaction(function () use($wd) {
-                            $information = "Conversion reward $ ".$this->amount." to ".($this->heba - $wd). " HEBA";
-                            $id = bitcoind()->getaccountaddress(auth()->user()->username).date('Ymdhis').round(microtime(true) * 1000);
-
-                            $transaksi = new Transaction();
-                            $transaksi->transaction_id = $id;
-                            $transaksi->transaction_information = $information." by ".auth()->user()->member_user;
-                            $transaksi->save();
-
-                            $trx_exchange = new TransactionExchange();
-                            $trx_exchange->rate_id = 1;
-                            $trx_exchange->transaction_exchange_type = "Reward";
-                            $trx_exchange->transaction_exchange_amount = $this->amount;
-                            $trx_exchange->transaction_id = $id;
-                            $trx_exchange->member_id = auth()->id();
-                            $trx_exchange->save();
-                        });
-                        redirect("/");
-
+                    $this->error = $response['errors']? $response['errors'][0]: 'Conversion failed. Try again later';
+                    return;
                 } catch (\Throwable $th) {
                     $this->error = $th->getMessage();
                 }
